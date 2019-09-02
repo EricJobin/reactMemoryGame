6b0d2c12d05ece1b3fd610413851f4489e1721d1
Eric Jobin <ejobin@gmail.com>
2019-09-01 23:16:25 -0400

    I don'tknow how to pass in a function, but I have to submit something for homework

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import Wrapper from "./components/Wrapper";
-
 import mem from "./mem.json"
-
 import MemCard from "./components/MemCard";
 
-function App() {
-	return (
-		<Wrapper>
-			{mem.map(
-				f => (<MemCard {...f} />)
-			)}
-		</Wrapper>
-	);
+class App extends Component {
+	state = {
+		chosen: [],
+		score: 0,
+	};
+
+	cardClick = () =>{
+		// console.log(event);
+		// console.log("click");
+		// console.log(this);
+
+
+
+	}
+
+	render() {
+		return (
+			<Wrapper>
+				{/* {console.log(this)} */}
+				{mem.map(
+					
+
+					// <MemCard
+					// 	id={mem.id}
+					// 	alt={mem.alt}
+					// 	image={mem.image}
+					// 	// cardClick={this.cardClick}
+					// />
+					// console.log(App.cardClick),
+					(f, {cardClick}) => (<MemCard {...f} />)
+				)}
+			</Wrapper>
+		);
+	}
 }
 
 export default App;
+
+
--- memory-game/src/components/MemCard.js
+++ memory-game/src/components/MemCard.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./style.css";
 
-function MemCard(props) {
+const MemCard = (props, fn) => {
+	// console.log(fn)
+
 	return (
 		<div className="card" id={props.id}>
-			{console.log(props)}
+			{/* {console.log(props)} */}
 			{/* {console.log(props.data.name)} */}
 			<div className="img-container">
-				<img src={props.image}/>
+				<img src={props.image} alt={props.alt}
+					// onClick={() => cardClick()}
+				/>
 			</div>
 		</div>
 	);
