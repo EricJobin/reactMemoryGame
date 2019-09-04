e335318a44f112adb03831268c58c773c5efe63d
Eric Jobin <ejobin@gmail.com>
2019-09-03 22:11:41 -0400

    Updates

---
+++ precache-manifest.eda450b9fc82f7a2e03e64d154be0d43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3e73d3a662480116ce32bcdced6c26",
+    "url": "/reactMemoryGame/index.html"
+  },
+  {
+    "revision": "9a3e2a824f790c9b5e03",
+    "url": "/reactMemoryGame/static/css/main.eb7defab.chunk.css"
+  },
+  {
+    "revision": "fa0044fd5427be8c1853",
+    "url": "/reactMemoryGame/static/js/2.f1bf8876.chunk.js"
+  },
+  {
+    "revision": "9a3e2a824f790c9b5e03",
+    "url": "/reactMemoryGame/static/js/main.f3e61b1c.chunk.js"
+  },
+  {
+    "revision": "9da58a5acc8fae8938db",
+    "url": "/reactMemoryGame/static/js/runtime~main.2ec85145.js"
+  }
+]);
